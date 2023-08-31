9555f419b81c1c14b890c989fef1767ffca54fdd
FernandoA5 <fernandoalcalacasas@hotmail.com>
2023-08-30 23:20:55 -0600

    Agregado código para listas buenas y malas, falta pulir

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
                 continue;
             }
         }
-        //Creamos 
+        //Creamos la lista de casos
         let mut lista_de_casos: Vec<Caso> = Vec::new();
         //Recorremos la lista de casos 
         for j in i..casos.len(){
@@ -63,17 +63,73 @@ fn main() {
         lista_de_listas_de_casos.push(lista_de_casos);
     }
     println!("Llegamos hasta aquí");
-    //Imprimimos la lista de listas
-    for lista_de_casos in lista_de_listas_de_casos {
-        for caso in lista_de_casos {
-            print!("[{}]", caso.case_id);
-        }
-        println!("");
-    }
+    
+    // //Imprimimos la lista de listas
+    // for lista_de_casos in lista_de_listas_de_casos {
+    //     for caso in lista_de_casos {
+    //         print!("[{}]", caso.case_id);
+    //     }
+    //     println!("");
+    // }
 
     let mut lista_buena: Vec<Caso> = Vec::new();
     let mut lista_mala: Vec<Caso> = Vec::new();
 
+    //Empecemos con la lista mala, aquí pondremos todos los casos que solo tengan una actividad
+    for lista_de_casos in lista_de_listas_de_casos.clone() {
+        if lista_de_casos.len() == 1 {
+            lista_mala.push(lista_de_casos[0].clone());
+        } else {
+            //Aquí debemos seguir filtrando:
+            //Temporalmente daremos por buenos aquellos que contengan por lo menos una Handle activity
+            //Recorremos la lista de listas de casos 
+            for lista_de_casos in lista_de_listas_de_casos.clone() {
+                //Recorremos la lista de casos
+                let mut valida = false;
+                for caso in lista_de_casos.clone() {
+                    //Si el caso es una handle activity
+                    if caso.activity.contains("Handle") {
+                        //Damos la lista por buena
+                        valida = true;
+                        break;
+                    }
+                }
 
+                //Si la lista es valida, la agregamos a la lista buena
+                if valida {
+                    for caso in lista_de_casos {
+                        //Revisamos el el unique_id no se encuentre ya en la lista buena
+                        for caso_bueno in lista_buena.clone() {
+                            if caso_bueno.unic_id == caso.unic_id {
+                                continue;
+                            }
+                        }
+                        lista_buena.push(caso);
+                    }
+                } else {
+                    //Si no, la agregamos a la lista mala
+                    for caso in lista_de_casos {
+                        //Revismos que el unique_id no se encuentre ya en la lista mala
+                        for caso_malo in lista_mala.clone() {
+                            if caso_malo.unic_id == caso.unic_id {
+                                continue;
+                            }
+                        }
+                        lista_mala.push(caso);
+                    }
+                }
+            }
+        }
+    }
+
+    //Imprimimos la lista mala
+    for caso in lista_mala {
+        println!("Caso: {}", caso.case_id);
+    }
+    //Imprimimos la lista buena
+    println!("Lista buena");
+    for caso in lista_buena {
+        println!("Caso: {} | Activity: {}", caso.case_id, caso.activity);
+    }
 
 }
